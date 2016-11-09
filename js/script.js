e75d05a73392c3f775af3826bb509743ccb9bf18
nataivanova <natalyas.ivanova@gmail.com>
2016-11-09 22:51:12 +0200

    Remove js/folder

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var test = {
-    createHeading : function() {
-        var heading = document.createElement("h1");
-        document.body.appendChild(heading);
-        heading.innerText = "Тест по программированию";
-    }
-}
-
-test.createHeading();
